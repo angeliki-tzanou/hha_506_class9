4def8ff99ced5c24c6596190bc27f21459756294
angeliki-tzanou <angeliki.tzanou@stonybrook.edu>
2023-08-19 00:37:12 +0000

    updated with variables

--- health-analytics.py
+++ health-analytics.py
@@ -1,7 +1,22 @@
-import numpy as np
+# Number variable
+number_variable = 3
 
-np.greetings("Ang")
+# String variable
+string_variable = "Hello my name is Angeliki but people call me Ang"
 
-import pandas as pd
+# List variable- birthday
+A_list = [8, 3, 2001]
 
-pd.greetings("A")
+# Dictionary
+my_dictionary = {
+    "Patient's name": "Jane Doe",
+    "pt age": 33,
+    "chief complaints": ["coughing", "shortness of breath", "chest pain"],
+    " family history": {
+        "maternal aunt": "lung cancer at 55 years old",
+        "paternal aunt": "COPD",
+        "sister": "adenocarcinoma at 78 years old"
+    }
+
+}
+
